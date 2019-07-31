0c93a14af2fe8aaff26d6f9e6d50bda924c9903d
ALopez201 <al234@njit.edu>
2019-07-31 00:00:14 -0400

    incorporated additional blade file for about page.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome to Anthony's Home Page
+                    About
                 </div>
 
                 <div class="links">
