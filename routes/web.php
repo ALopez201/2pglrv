d2ccf9ac3f0f62ecb8070dd61bdbcad4dd893d4b
ALopez201 <al234@njit.edu>
2019-08-01 17:40:54 -0400

    created a template using the moodle tutorial link.

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,20 @@
 |
 */
 
-Route::get('/', function () {
-    return view('pages.home');
+Route::get('/', function()
+{
+    return View::make('pages.home');
+});
+Route::get('about', function()
+{
+    return View::make('pages.about');
+});
+Route::get('projects', function()
+{
+    return View::make('pages.projects');
+});
+Route::get('contact', function()
+{
+    return View::make('pages.contact');
 });
 
-
-Route::get('/about', function () {
-    return view('pages.about');
-});
